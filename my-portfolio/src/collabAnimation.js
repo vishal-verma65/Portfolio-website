c7f6024f02c1156c64e7bc7c2b739ab0130988d4
vishalvrma.404@gmail.com <vishalvrma.404@gmail.com>
2025-07-04 23:04:54 +0530

    Collaboration section animation completed

---
+++ my-portfolio/src/collabAnimation.js
@@ -0,0 +1,81 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
+gsap.registerPlugin(ScrollTrigger, ScrambleTextPlugin);
+
+export const collabAnimation = ()=>{
+
+    //slider animation
+    gsap.to(".left-slide", {
+        x: "-100%",
+        ease: "none",
+        duration: 15,
+        repeat: -1,
+    });
+
+    gsap.to(".right-slide", {
+        x: "0%",
+        ease: "none",
+        duration: 15,
+        repeat: -1,
+    });
+
+    // memorable word change animation
+    function wordChange(){
+        gsap.to(".memorable-span",{
+            delay: 3,
+            duration: 2,
+            scrambleText: {
+                text: "iconic",
+                chars: "XO",
+                speed: 1,
+            },
+            conComplete: reChange,
+        });
+    };
+    wordChange();
+    function reChange(){
+        gsap.to(".memorable-span",{
+            delay: 6,
+            duration: 2,
+            scrambleText: {
+                text: "memorable",
+                chars: " ",
+                speed: 1,
+                },
+            onComplete:  wordChange,
+
+        });
+    };
+
+    //together word change animation
+    function togetherChange(){
+        gsap.to(".together-span",{
+            delay: 2,
+            duration: 2,
+            scrambleText: {
+                text: "as partners",
+                chars: " ",
+                speed: 1,
+            },
+            conComplete: reChangeTogether,
+        });
+    };
+    togetherChange();
+    function reChangeTogether(){
+        gsap.to(".together-span",{
+            delay: 4,
+            duration: 2,
+            scrambleText: {
+                text: "together",
+                chars: "XO",
+                speed: 1,
+                },
+            onComplete: togetherChange,
+
+        });
+    };
+
+    
+
+};
--- my-portfolio/src/main.js
+++ my-portfolio/src/main.js
@@ -1,7 +1,9 @@
 import { gsap } from "gsap";
 import { heroAnimation } from "./heroAnimation";
 import { aboutAnimation } from "./aboutAnimation";
+import { collabAnimation } from "./collabAnimation";
 
 heroAnimation();
 aboutAnimation();
+collabAnimation();
 
