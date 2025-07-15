7f16b5fd88b18e5079cbd8563f51a5bb3e267751
vishalvrma.404@gmail.com <vishalvrma.404@gmail.com>
2025-07-15 17:37:07 +0530

    About section responsiveness completed

--- my-portfolio/src/aboutAnimation.js
+++ my-portfolio/src/aboutAnimation.js
@@ -4,9 +4,83 @@ gsap.registerPlugin(ScrollTrigger);
 
 export const aboutAnimation = ()=>{
     if(window.innerWidth < 1024){
-        return false;
+        const tl = gsap.timeline();
+
+        tl.from(".about-me",{
+            y: 100,
+            autoAlpha: 0,
+            scrollTrigger: {
+                trigger: ".about-me",
+                start: "top 90%",
+                end: "top 50%",
+                scrub: 1,
+            }
+        })
+        .from(".about-clip",{
+            x: -300,
+            rotate: -20,
+            y: 50,
+            scrollTrigger: {
+                trigger: ".about-clip",
+                start: "top 80%",
+                end: "top 50%",
+                scrub: 5,
+            }
+        }, "=")
+        .from(".tech-stack",{
+            y: 100,
+            autoAlpha: 0,
+            scrollTrigger: {
+                trigger: ".tech-stack",
+                start: "top 80%",
+                end: "top 45%",
+                scrub: 2,
+            }
+        })
+        .to(".tech-stack-flower",{
+            x: 200,
+            rotate: 360,
+            scrollTrigger: {
+                trigger: ".tech-stack-flower",
+                start: "top 70%",
+                end: "top 20%",
+                scrub: 2,
+            }
+        }, "=")
+        .from(".tools",{
+            y: 100,
+            autoAlpha: 0,
+            scrollTrigger: {
+                trigger: ".tools",
+                start: "top 80%",
+                end: "top 45%",
+                scrub: 2,
+            }
+        })
+        .from(".availability",{
+            y: 100,
+            autoAlpha: 0,
+            scrollTrigger: {
+                trigger: ".availability",
+                start: "top 80%",
+                end: "top 45%",
+                scrub: 2,
+            }
+        })
+        .from(".earth-clip",{
+            x: 200,
+            rotate: 360,
+            scrollTrigger: {
+                trigger: ".earth-clip",
+                start: "top 60%",
+                end: "top 20%",
+                scrub: 3,
+            }
+        }, "=")
+
     }
 
+    else{
     //pinning the about main wrapper 
     gsap.to(".about-section .about-main-wrapper",{
         transform: "translate(-74.5%)",
@@ -71,5 +145,6 @@ export const aboutAnimation = ()=>{
             start: "top top",
             end: "500%",
             }
-        });       
+        });
+    }           
 };
